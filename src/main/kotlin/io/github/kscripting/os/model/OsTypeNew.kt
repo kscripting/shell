4c495e2dcce6c59825c5b33613de6660b60b80c3
Marcin Kuszczak <1508798+aartiPl@users.noreply.github.com>
2023-08-19 17:17:33 +0200

    Design cd.

--- src/main/kotlin/io/github/kscripting/os/model/OsTypeNew.kt
+++ src/main/kotlin/io/github/kscripting/os/model/OsTypeNew.kt
@@ -2,35 +2,12 @@ package io.github.kscripting.os.model
 
 import io.github.kscripting.os.Os
 import io.github.kscripting.os.instance.*
+import net.igsoft.typeutils.enum.TypedEnumCompanion
 import net.igsoft.typeutils.globalcontext.GlobalContext
 import net.igsoft.typeutils.marker.AutoTypedMarker
 import net.igsoft.typeutils.marker.TypedMarker
 import org.apache.commons.lang3.SystemUtils
-import kotlin.properties.ReadOnlyProperty
 
-
-abstract class TypedEnumCompanion<T> {
-    private val registry: MutableMap<String, T> = mutableMapOf()
-
-    fun find(name: String): T? = registry[name]
-    fun findOrThrow(name: String): T =
-        find(name) ?: throw IllegalArgumentException("Can not find enum value for: '$name'")
-
-    protected fun <V : T> register(name: String, instance: V): V {
-        registry[name] = instance
-        return instance
-    }
-
-    protected fun <V : T> register(instance: V): ReadOnlyProperty<Any?, V> {
-        return ReadOnlyProperty { _, property ->
-            registry[property.name] = instance
-            instance
-        }
-    }
-
-    protected fun findName(instance: T) =
-        registry.entries.firstOrNull { it.value == instance }?.key ?: error("Enum not registered")
-}
 
 class OsTypeNew<T : Os> private constructor(private val marker: TypedMarker<T>) : TypedMarker<T> {
     val value get() = GlobalContext.getValue(marker)
